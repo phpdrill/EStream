2dd4969bf53138ee0170003844afd39a22e69fd6
judos <judos@gmx.ch>
2015-08-31 20:50:49 +0200

    added receiver

---
+++ src/Receiver.java
@@ -0,0 +1,78 @@
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+import ch.judos.generic.network.udp.UdpLib;
+import ch.judos.generic.network.udp.interfaces.FileTransmissionHandler;
+import ch.judos.generic.network.udp.interfaces.Udp4I;
+import ch.judos.generic.network.udp.interfaces.UdpFileTransferListener;
+import ch.judos.generic.network.udp.interfaces.UdpListener;
+import ch.judos.generic.network.udp.model.FileDescription;
+
+/**
+ * @since 31.08.2015
+ * @author Julian Schelker
+ */
+public class Receiver implements FileTransmissionHandler, UdpFileTransferListener, UdpListener {
+
+	public static void main(String[] args) throws SocketException {
+		new Receiver();
+	}
+
+	public Receiver() throws SocketException {
+		Udp4I udpLib = UdpLib.createOnPort(25565);
+
+		udpLib.setFileHandler(this);
+
+		udpLib.addObjectListener(this);
+	}
+
+	@Override
+	public File requestFileTransmission(FileDescription fd) {
+		System.out.println(fd.getDescription());
+		System.out.println(fd.getPath());
+		System.out.println("Length: " + fd.getLength());
+		System.out.println("Parts: " + fd.getParts());
+		return null;
+	}
+
+	@Override
+	public UdpFileTransferListener requestTransferFileListener(FileDescription fd) {
+		return this;
+	}
+
+	@Override
+	public int getUpdateEveryMS() {
+		return 1000;
+	}
+
+	@Override
+	public void transmissionAcceptedAndStarted() {
+		System.out.println("Accepted and started");
+	}
+
+	@Override
+	public void transmissionCompleted() {
+		System.out.println("Completed");
+	}
+
+	@Override
+	public void transmissionDeniedAndCanceled() {
+		System.out.println("Denied and canceled");
+	}
+
+	@Override
+	public void transmissionProgress(float percentage, float avgSpeed, long transmitted,
+		long total) {
+		System.out.println(percentage + "% avgSpeed: " + avgSpeed + "  progress: "
+			+ transmitted + "/" + total);
+	}
+
+	@Override
+	public void receiveMsg(Object source, InetSocketAddress from, Object data) {
+		System.out.println("received object from: " + source);
+		System.out.println("from: " + from);
+		System.out.println("data: " + data);
+	}
+
+}
