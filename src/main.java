e0ed2d4cdea3bdfd84be40e81da706ebf8e8502d
Alessio <Alessio@192.168.1.132>
2015-08-31 19:47:46 +0200

    Simple Main Method

--- src/main.java
+++ src/main.java
@@ -1,8 +1,13 @@
+import javax.swing.JOptionPane;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "Test", "Test Titel", JOptionPane.OK_CANCEL_OPTION);
+		
+
 
 	}
 
