c17b7da075a1f4407e97b825d6cf4e9a04b154dc
judos <judos@gmx.ch>
2015-09-03 23:18:31 +0200

    fix merge conflict bug

--- src/controller/HostListController.java
+++ src/controller/HostListController.java
@@ -4,57 +4,62 @@ import java.io.InputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.xml.sax.SAXException;
-
 import model.Host;
 import model.HostList;
+
+import org.xml.sax.SAXException;
+
 import util.URLConnector;
 import view.SelectionListener;
 import view.ViewApi;
 
 public class HostListController implements SelectionListener<Host> {
-	
+
 	private HostList hostList = null;
 	private ViewApi api;
 	public HostListController(ViewApi api) {
 		this.api = api;
 	}
 
-	public void show(){
-		
-		if(hostList == null) get();
-		
-		
+	public void show() {
+
+		if (hostList == null)
+			get();
+
 		api.setSelectionListener(this);
 		api.setListOfHosts(hostList);
-		
+
 	}
-	
-	private void get(){
-		
-		try{
-			
-			InputStream content = URLConnector.getContent("http://www.however.ch/estream/getHostList.php");
+
+	private void get() {
+
+		try {
+
+			InputStream content = URLConnector
+				.getContent("http://www.however.ch/estream/getHostList.php");
 			hostList = HostList.getFromString(content);
-			
-		}catch(ParserConfigurationException exception){
+
+		}
+		catch (ParserConfigurationException exception) {
 			exception.printStackTrace();
-		}catch(SAXException exception){
+		}
+		catch (SAXException exception) {
 			exception.printStackTrace();
-		}catch(IOException exception){
+		}
+		catch (IOException exception) {
 			exception.printStackTrace();
 		}
 	}
 
 	@Override
 	public void selected(Host host) {
-		
-		String path = host.nameIs("Alessio")
-				? "D:\\Musik\\25.08.2015" //*** PFAD ALESSIO ***//
-				: ""; //*** PFAD JULIAN ***//
-		
+
+		// *** PFAD ALESSIO ***//
+		String path = host.getName().equals("Alessio") ? "D:\\Musik\\25.08.2015" : "";
+		// *** PFAD JULIAN ***//
+
 		FileListController fileListC = new FileListController();
 		fileListC.show(path, host, api);
-		
+
 	}
 }
