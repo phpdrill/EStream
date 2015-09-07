c82181d68691395711e55fe9fa7b96cc4a32d987
Alessio <Alessio@192.168.1.132>
2015-09-07 19:53:57 +0200

    registerHost.php --> Port and ip added

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -6,9 +6,15 @@ import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import ch.judos.generic.network.udp.Udp4Forwarded;
+import ch.judos.generic.network.udp.UdpLib;
+import ch.judos.generic.network.udp.interfaces.Udp4I;
 import sun.misc.IOUtils;
 import util.URLConnector;
 import view.ViewApi;
@@ -16,12 +22,15 @@ import view.ViewApi;
 public class MainController {
 	
 	private ViewApi api;
+	private Timer timer;
+	private Udp4Forwarded udp;
 	
-	public MainController(){
+	public MainController() throws SocketException{
 		
 		api = new ViewApi();
+		udp = UdpLib.createForwarded();
 		
-		registerHost();
+		createRegisterHostTimer();
 		
 		HostListController hostListC = new HostListController(api);
 		hostListC.show();
@@ -31,16 +40,28 @@ public class MainController {
 
 	private void registerHost() {
 		
+		
 		String name = System.getProperty("user.name");
-		String ip = "blabli";
-		String port = "blabla";
+		int port = udp.getExternalPort();
 		
 		InputStream inputStream = 
 				URLConnector.getContent("http://www.however.ch/estream/registerHost.php?"
 				+ "name=" + name
-				+ "&ip=" + ip
 				+ "&port=" + port);
 
+		
+	}
+
+	private void createRegisterHostTimer() {
+		
+		timer = new Timer();
+		TimerTask task = new TimerTask(){
+			@Override
+			public void run(){
+				registerHost();
+			}
+		};
+		timer.schedule(task, 0l, 1000*10);
 		
 	}
 
